aa028bbe18708ea513e8341d5f88b02d274ddac6
Duru Baştunalı <durubastunali@gmail.com>
2024-08-06 09:31:56 +0300

    06.08.2024

--- src/main/java/com/example/internduru/Launcher.java
+++ src/main/java/com/example/internduru/Launcher.java
@@ -4,6 +4,7 @@ import com.example.internduru.features.StageHandler;
 import javafx.application.Application;
 
 public class Launcher { //lazım olabilir belki??
+    // bazı yerlerlede java fx jarı için herhangi bir şey extendlemeyen main'den maini çağıran class lazım diyorlar -> javafx components missing errorunu çözüyor
     public static void main(String[] args) {
         Application.launch(StageHandler.class, args);
     }
